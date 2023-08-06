60da722a887cf54f2023ea25b15725b14a7a388f
Aditya Saini <Adityasaini10012001@outlook.com>
2023-08-06 15:14:59 +0530

    Learning About API'S and Database
    Signed-off-by: Aditya Saini <Adityasaini10012001@outlook.com>

---
+++ Routes/Product.route.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const router = express.Router();
+
+const Product = require('../Models/Product.model')
+
+router.get('/',(req,res,next) => {
+    next(new Error("cannot get a list of all products"))
+    // res.send('getting a list of all products..');
+});
+
+// async method 
+router.post('/', async(req,res,next) => {
+
+    try {
+    const product = new Product(req.body);
+    const result = await product.save()
+    res.send(result)
+
+    } catch (error) {
+        console.log(error.message);
+    }
+
+  
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // router.post('/', (req,res,next) => {
+    // console.log(req.body);
+    // Promise method 
+    // const product = new Product({
+    //     name: req.body.name,
+    //     price: req.body.price
+    // });
+    // product.save()
+    //   .then(result => {
+    //     console.log(result);
+    //     res.send(result);
+
+    //   })
+    //   .catch(err => {
+    //     console.log(err.message);
+
+    //   })
+    // res.send('product created');
+});
+
+router.get('/:id', (req,res,next) =>{
+    res.send('getting a single product')
+
+});
+
+router.patch('/:id', (req,res,next) =>{
+    res.send('Updating a single product')
+    
+});
+
+router.delete('/:id', (req,res,next) =>{
+    res.send('deleting a single product')
+    
+});
+
+
+
+module.exports = router;
---
+++ app.js
@@ -0,0 +1,170 @@
+// without express
+// const http = require('http')
+
+// const server = http.createServer((req,res) => {
+//     if(req.url === '/') {
+//         if(req.method === "GET") {
+//             console.log("Its a GET method")
+//         }
+//         res.write('I am listening on home page');
+//         res.end();
+//     }
+//     else if(req.url === '/another') {
+//         res.write('I am listening on / another');
+//         res.end();
+
+//     }
+//     else
+//     {
+//     res.write("I am listening ");
+//     res.end();
+//     }
+
+// });
+
+
+// server.listen(3000, () => {
+//     console.log('server started on port 3000...')
+// });
+
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+// mongodb+srv://sainiaditya1:<password>@cluster0.r0rcu1h.mongodb.net/?retryWrites=true&w=majority
+
+
+const app = express();
+// RESTAPI is the database name
+// mongoose.connect("mongodb://localhost:27017/RESTAPI", { useNewUrlParser: true, useUnifiedTopology: true } );// Adding this option is recommended for removing deprecation warning.
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+// ?retryWrites=true&w=majority
+mongoose
+    .connect('mongodb+srv://cluster0.r0rcu1h.mongodb.net/',
+    { 
+        dbName: 'restapi',
+        user:'sainiaditya1',
+        pass: 'jooH0pRtBixP1iBK' ,
+        useNewUrlParser: true, 
+        useUnifiedTopology: true })
+    .then(() => {
+        // const app = express()
+
+        // app.listen(3000,() => {
+            console.log('mongodb connected')
+        // })
+
+    });
+
+
+
+
+
+
+
+
+
+// to connect locally
+// mongoose
+//     .connect("mongodb://localhost:27017/RESTAPI",{useNewUrlParser: true, useUnifiedTopology: true})
+//     .then(() => {
+//         // const app = express()
+
+//         // app.listen(3000,() => {
+//             console.log('mongodb connected')
+//         // })
+
+//     })
+
+
+
+
+// app.all('/test/:id/:name', (req,res) => {
+    // console.log(req.query);
+    // console.log(req.query.name);
+    // res.send(req.query);
+
+    // console.log(req.params);
+    // res.send(req.params);
+
+app.all('/test', (req,res) => {
+
+    console.log(req.body);
+    res.send(req.body);
+
+});
+
+
+
+
+
+
+
+
+const ProductRoute = require('./Routes/Product.route');
+
+app.use('/products',ProductRoute);
+app.use((req,res,next) =>{
+    
+    const arr = new Error("Not found")
+    err.status = 404
+    next(err)
+
+});
+
+// error handler
+// 500 for internal server error
+
+app.use((err,req,res,next) => {
+    res.status(err.status || 500);
+    res.send({
+        error: {
+            status: err.status || 500,
+            message: err.message
+
+            }
+        })
+    })
+
+
+
+
+
+
+
+
+
+
+
+// app.get('/products',(req,res,next) => {
+
+// })
+
+// app.get('/',(req,res,next) => {
+//     console.log(req.url)
+//     console.log(req.method)
+//     res.send("I am the home route")
+// });
+
+// app.post('/',(req,res,next) =>{
+
+
+// })
+
+// app.delete('/',(req,res,next) => {
+
+// })
+
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000...');
+});
+
+
+
+
+
+// jooH0pRtBixP1iBK
